1fa9707444643eb03c02b0773a48602978af8e7c
Andrew Jia <andrew.jia.2013@gmail.com>
2022-04-23 13:37:43 -0700

    Update botTest.js
    commands work now :)

--- botTest.js
+++ botTest.js
@@ -11,10 +11,18 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
-  }
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
 });
 
 client.login(process.env.TOKEN);
