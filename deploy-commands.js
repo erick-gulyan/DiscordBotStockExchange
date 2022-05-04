4f53e5eebd6e8748b730750daf17a5bf17c6b113
Andrew Jia <andrew.jia.2013@gmail.com>
2022-05-04 15:09:34 -0700

    Update deploy-commands.js
    added stocktest command, basically !checkprice in command form

--- deploy-commands.js
+++ deploy-commands.js
@@ -10,10 +10,16 @@ const clientId = process.env.CLIENTID;
 const guildId = process.env.GUILDID;
 const token = process.env.TOKEN;
 
+
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+	new SlashCommandBuilder().setName('stocktest').setDescription('gets price of a stock')
+	.addStringOption(option =>
+		option.setName('input')
+			.setDescription('stock ticker')
+			.setRequired(true))
 ]
 	.map(command => command.toJSON());
 
